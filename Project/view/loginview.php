a125e883d792f42f73030031226afd5936ec6d4c
lordmcfungus <alain.keller@edu.tbz.ch>
2016-12-13 09:23:53 +0100

    #24 Login "GUI" erstelt

---
+++ Project/view/loginview.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alain
+ * Date: 13.12.2016
+ * Time: 09:09
+ */
+?>
+
+<html>
+    <head>
+        <title>Login</title>
+        <script src="login.js"></script>
+    </head>
+    <body>
+        <form onsubmit="login()">
+            <label for="username">Username</label> <input type="text" name="username" id="username"> <br/>
+            <label for="password">Password</label> <input type="password" name="password" id="password"> <br />
+            <input type="submit" name="loginbutton" id="loginbutton">
+        </form>
+    </body>
+</html>
