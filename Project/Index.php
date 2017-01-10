a458ebdee8fd0e2de7dad64bed97271d73bce9c4
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-01-10 11:34:06 +0100

    #19: Navigation implementiert

--- Project/Index.php
+++ Project/Index.php
@@ -18,27 +18,35 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.print.css">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js"></script>
 
+        <script src="js/index.js"></script>
+
         <title>Aljogi</title>
     </head>
     <body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">WebSiteName</a>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#" id="home" onclick="activateTab('home', 'view/Home.php')" data-toggle="tab">Home</a></li>
+                    <li><a href="#" id="calendar" onclick="activateTab('calendar', 'view/CalendarView.php')" data-toggle="tab">Calendar</a></li>
+                    <li><a href="#" id="posts" onclick="activateTab('posts', 'view/posts.php')" data-toggle="tab">Posts</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a onclick=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a onclick=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
             </div>
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a onclick=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a onclick=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            </ul>
+        </nav>
+
+        <div id="mainContent">
+
         </div>
-    </nav>
     </body>
  </html>
