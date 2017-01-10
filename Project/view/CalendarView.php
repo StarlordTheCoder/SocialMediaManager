00cc356fd465dc04938dac5fd56007d7380d2b82
Seraphin Elias Rihm <seraphin.rihm@edu.tbz.ch>
2017-01-10 11:35:41 +0100

    #19

---
+++ Project/view/CalendarView.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by Notepad++.
+ * User: Seraphin
+ * Date: 10.01.2017
+ * Time: 09:17
+ */	
+?>
+
+<script>
+
+	$(document).ready(function() {
+		
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2014-06-12',
+			defaultView: 'agendaWeek',
+			editable: true,
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2014-06-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2014-06-07',
+					end: '2014-06-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-06-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2014-06-16T16:00:00'
+				},
+				{
+					title: 'Meeting',
+					start: '2014-06-12T10:30:00',
+					end: '2014-06-12T12:30:00'
+				},
+				{
+					title: 'Lunch',
+					start: '2014-06-12T12:00:00'
+				},
+				{
+					title: 'Birthday Party',
+					start: '2014-06-13T07:00:00'
+				},
+				{
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2014-06-28'
+				}
+			]
+		});
+		
+	});
+
+</script>
+
+<style type="text/css">
+
+	#calendar {
+		width: 900px;
+		margin: 0 auto;
+	}
+
+</style>
+
+<div id='calendar'></div>
