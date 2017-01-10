377264c0bbaf97061cf3eb0b35613b4cc4979461
lordmcfungus <alain.keller@edu.tbz.ch>
2017-01-10 08:54:26 +0100

    #44 navbar

---
+++ Project/Index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alain
+ * Date: 10.01.2017
+ * Time: 08:37
+ */
+?>
+ <html>
+    <head>
+        <title>Aljogi</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script src="../js/register.js" type="application/javascript"></script>
+        <script src="../js/jquery.min.js" type="application/javascript"></script>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Page 1</a></li>
+                <li><a href="#">Page 2</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a onclick=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a onclick=""><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </nav>
+    </body>
+ </html>
---
+++ Project/view/MasterPage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alain
+ * Date: 10.01.2017
+ * Time: 08:38
+ */
+?>
+
+
+<div>
+
+</div>
