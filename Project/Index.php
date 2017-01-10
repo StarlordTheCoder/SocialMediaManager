13a40c9052e7437a5cdd5b589a12feb4e1bd5e91
lordmcfungus <alain.keller@edu.tbz.ch>
2017-01-10 09:25:57 +0100

    #44 Postview Tabs

--- Project/Index.php
+++ Project/Index.php
@@ -23,7 +23,6 @@
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
                 <li><a href="#">Page 1</a></li>
-                <li><a href="#">Page 2</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a onclick=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
---
+++ Project/view/PostView.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alain
+ * Date: 10.01.2017
+ * Time: 09:21
+ */
+
+?>
+
+<ul class="nav nav-tabs">
+    <li class="active"><a onclick="">Create Post</a></li>
+    <li><a onclick="">Show Posts</a></li>
+</ul>
